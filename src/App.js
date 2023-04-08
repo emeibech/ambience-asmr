06b8ee262876f58ad327e4e4b8bff8d53a8fa596
emeibech <emeibech@gmail.com>
2023-04-08 15:57:13 +0800

    Modify App.js to test seagull sound

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import AudioControl from './components/AudioControl';
+import seagullTest from './assets/sounds/seagulls-test.mp3'
 
 const App = () => {
   return (
     <div className="App">
-      <p>Under Construction</p>
+      <AudioControl source={seagullTest} name='Seagull Test'></AudioControl>
     </div>
   );
 }
