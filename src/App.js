8bc535e146f9eb2dca6c460616d022c126491a6f
emeibech <emeibech@gmail.com>
2023-04-15 02:40:11 +0800

    Refactor App component to accomodate new structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import './App.css';
-import AudioControl from './components/AudioControl';
-import seagullTest from './assets/sounds/seagulls-test.mp3'
+import AudioControl from './components/AudioControl/AudioControl';
+import campfire from './assets/sounds/crackling-campfire.ogg';
+import gentleWaves from './assets/sounds/gentle-waves.ogg';
+import splash from './assets/sounds/splashing-water.ogg';
+import strongWaves from './assets/sounds/strong-waves.ogg';
+import wind from './assets/sounds/wind.ogg';
+import wading from './assets/sounds/wading-in-the-water.ogg';
 
 const App = () => {
+
   return (
     <div className="App">
-      <AudioControl source={seagullTest} name='Seagull Test'></AudioControl>
+      <div className='audio-container'>
+        {/* <AudioControl source={seagullTest} name='Seagull Test'></AudioControl> */}
+        <AudioControl source={gentleWaves} name='Gentle Waves'>
+        </AudioControl>
+
+        <AudioControl source={strongWaves} name='Strong Waves'>
+        </AudioControl>
+
+        <AudioControl source={splash} name='Splashing Water'>
+        </AudioControl>
+
+        <AudioControl source={wind} name='Wind'>
+        </AudioControl>
+
+        <AudioControl source={campfire} name='Campfire'>
+        </AudioControl>
+
+        <AudioControl source={wading} name='Wading in the Water'>
+        </AudioControl>
+      </div>
     </div>
   );
 }
