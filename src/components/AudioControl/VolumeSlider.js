f8661b6c374b2bd00cad2b53d8cba83cddd8f42b
emeibech <emeibech@gmail.com>
2023-04-17 18:24:34 +0800

    Add lower fill on the volume slider

--- src/components/AudioControl/VolumeSlider.js
+++ src/components/AudioControl/VolumeSlider.js
@@ -1,24 +1,44 @@
-import './VolumeSlider.css'
+import "./VolumeSlider.css";
 import React, { useState } from "react";
 
 const VolumeSlider = ({ name }) => {
   const [volume, setVolume] = useState(50);
 
-  const changeVolume = (e) => {
+  const handleVolume = (e) => {
     const audio = document.querySelector(`[data-audio='${name}']`);
     setVolume(e.target.value);
     audio.volume = e.target.value / 100;
   };
 
+  const handleSlider = (e) => {
+    const handleInputChange = (e) => {
+      let target = e.target;
+
+      const min = target.min;
+      const max = target.max;
+      const val = target.value;
+
+      target.style.backgroundSize =
+        ((val - min) * 100) / (max - min) + "% 100%";
+    };
+
+    handleInputChange(e);
+  };
+
+  const handleOnChange = (e) => {
+    handleSlider(e);
+    handleVolume(e);
+  };
+
   return (
     <div>
       <input
-        id="volume-slider"
+        id={name}
         type="range"
         min="0"
         max="100"
         value={volume}
-        onChange={changeVolume}
+        onChange={handleOnChange}
       />
     </div>
   );
