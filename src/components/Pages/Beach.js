6f2c824e8fda70b92180420528e67d2430e1b041
emeibech <emeibech@gmail.com>
2023-05-03 15:29:55 +0800

    Move beach asmr page to its own component

---
+++ src/components/Pages/Beach.js
@@ -0,0 +1,36 @@
+import './Beach.css';
+import AudioControl from '../AudioControl/AudioControl';
+import campfire from '../../assets/sounds/crackling-campfire.ogg';
+import gentleWaves from '../../assets/sounds/gentle-waves.ogg';
+import splash from '../../assets/sounds/splashing-water.ogg';
+import strongWaves from '../../assets/sounds/strong-waves.ogg';
+import wind from '../../assets/sounds/wind.ogg';
+import wading from '../../assets/sounds/wading-in-the-water.ogg';
+
+const Beach = () => {
+  return (
+    <div className="beach">
+      <div className='audio-container'>
+        <AudioControl source={gentleWaves} name='Gentle Waves'>
+        </AudioControl>
+
+        <AudioControl source={strongWaves} name='Strong Waves'>
+        </AudioControl>
+
+        <AudioControl source={splash} name='Splashes'>
+        </AudioControl>
+
+        <AudioControl source={wind} name='Wind'>
+        </AudioControl>
+
+        <AudioControl source={campfire} name='Campfire'>
+        </AudioControl>
+
+        <AudioControl source={wading} name='Wading'>
+        </AudioControl>
+      </div>
+    </div>
+  );
+}
+
+export default Beach;
