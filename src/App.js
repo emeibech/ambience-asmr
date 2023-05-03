70b059a1694a3d88e656ccd7c9fa7179564a6fd0
emeibech <emeibech@gmail.com>
2023-05-03 15:28:54 +0800

    Modify structure to accomodate multiple asmr pages

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
-import './App.css';
-import AudioControl from './components/AudioControl/AudioControl';
-import campfire from './assets/sounds/crackling-campfire.ogg';
-import gentleWaves from './assets/sounds/gentle-waves.ogg';
-import splash from './assets/sounds/splashing-water.ogg';
-import strongWaves from './assets/sounds/strong-waves.ogg';
-import wind from './assets/sounds/wind.ogg';
-import wading from './assets/sounds/wading-in-the-water.ogg';
+import './App.css'
+import Beach from './components/Pages/Beach'
+import TabsPanel from './components/Tabs/TabsPanel';
 
 const App = () => {
-
   return (
-    <div className="App">
-      <div className='audio-container'>
-        <AudioControl source={gentleWaves} name='Gentle Waves'>
-        </AudioControl>
-
-        <AudioControl source={strongWaves} name='Strong Waves'>
-        </AudioControl>
-
-        <AudioControl source={splash} name='Splashes'>
-        </AudioControl>
-
-        <AudioControl source={wind} name='Wind'>
-        </AudioControl>
-
-        <AudioControl source={campfire} name='Campfire'>
-        </AudioControl>
-
-        <AudioControl source={wading} name='Wading'>
-        </AudioControl>
-      </div>
+    <div className='app'>
+      <Beach></Beach>
+      <TabsPanel></TabsPanel>
     </div>
   );
 }
