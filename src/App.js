8c547e74ae702231db0da9977cb4b938307b5ba9
emeibech <emeibech@gmail.com>
2023-05-10 06:44:17 +0800

    Restructure how state is handled

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import './App.css'
+import React, { useState, createContext } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import TabsPanel from './components/Tabs/TabsPanel';
-import Beach from './components/Pages/Beach'
+import PageControl from './components/Pages/PageControl';
+
+export const NavStatusContext = createContext();
 
 const App = () => {
+  const [navStatus, setNavStatus] = useState({
+    beach: true,
+    forest: false,
+    garden: false,
+    cafe: false,
+  });
+
   return (
-    <div className='app'>
-      <Navigation />
-      <Beach />
-      <TabsPanel />
+    <div className='App'>
+      <NavStatusContext.Provider value={setNavStatus}>
+        <Navigation navStatus={navStatus} />
+      </NavStatusContext.Provider>
+
+      <PageControl navStatus={navStatus} />
+
+      <NavStatusContext.Provider value={setNavStatus}>
+        <TabsPanel navStatus={navStatus} />
+      </NavStatusContext.Provider>
     </div>
   );
 }
